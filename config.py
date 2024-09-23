1cd604c0702b0b5e08c44415c4d6878dd0bfad8a
Naoyy <jyoan22@gmail.com>
2024-09-23 16:47:53 +0200

    add config

---
+++ config.py
@@ -0,0 +1,21 @@
+
+class Color:
+    pique='pique' 
+    coeur='coeur' 
+    trefle='trèfle' 
+    carreau= 'carreau'
+
+class Value: # aucune idée si ça servira par la suite
+    un="A"
+    deux="2"
+    trois="3"
+    quatre="4"
+    cinq="5"
+    six="6"
+    sept="7"
+    huit="8"
+    neuf="9"
+    dix="10"
+    onze="J"
+    douze="Q"
+    treize="K"
